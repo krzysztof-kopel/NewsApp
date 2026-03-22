2ae2c8c1282e4da61d41a77641718ea0fe452841
krzysbielsko <krzysbielsko@gmail.com>
2026-03-22 13:55:25 +0100

    Started implementing frontend for translations.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fastapi.params import Form
+
 import backend
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, FileResponse
@@ -21,7 +23,11 @@ async def top_articles(request: Request):
     request_results = backend.download_top()
     return process_results(request, request_results)
 
-def process_results(request: Request, results: dict, title: str | None = None) -> _TemplateResponse:
+@app.post("/translate", response_class=HTMLResponse)
+async def translate(request: Request, text_to_translate: str=Form()):
+    ...
+
+def process_results(request: Request, results: dict, title: str | None = None, translate: bool = True) -> _TemplateResponse:
     if title is None:
         title = "top articles"
     else:
@@ -29,7 +35,7 @@ def process_results(request: Request, results: dict, title: str | None = None) -
     if results["status"] == "ok":
         articles = results["articles"]
         return templates.TemplateResponse(
-            request=request, name="article_list.j2", context={"article_list": articles, "title": title}
+            request=request, name="article_list.j2", context={"article_list": articles, "title": title, "translate": translate}
         )
     else:
         return templates.TemplateResponse(
